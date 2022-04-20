fd1e4fd6cbe5ac0fc511e479f1d67f6666e91440
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-04-20 09:50:15 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { randomName } from './randomName'
+
+export {
+    randomName,
+}
---
+++ src/randomName.ts
@@ -0,0 +1,4 @@
+export const randomName = (): string => {
+    const names: Array<string> = ['Peter', 'Bob', 'Udo']
+    return names[0]
+}
